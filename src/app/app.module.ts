82b57cf4443aec2f31fbef365f31ffa2100837f7
hokira <hokira@gmail.com>
2018-01-28 16:03:14 -0500

    :

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,24 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireAuthModule } from 'angularfire2/auth';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDDepsC_uUsRI5ONlCN-678xcjsRyLkq2U",
+    authDomain: "ionic-test-cd963.firebaseapp.com",
+    databaseURL: "https://ionic-test-cd963.firebaseio.com",
+    projectId: "ionic-test-cd963",
+    storageBucket: "",
+    messagingSenderId: "178669050576"
+      }
+};
+  
 
 @NgModule({
   declarations: [
@@ -14,7 +29,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule, // imports firebase/auth, only needed for auth features,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,55 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { AngularFireAuth } from 'angularfire2/auth';
+import firebase from 'firebase';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  facebook = {
+    signedin: false,
+    name: '',
+    email: ''
+  }
+
+  google = {
+    signedin: false,
+    name: '',
+    email: ''
+  }
+
+  constructor(private fireAuth: AngularFireAuth, public navCtrl: NavController) {
 
   }
+
+  loginFacebook() {
+    console.log('signin')
+    this.fireAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider())
+    .then(res => {
+      console.log('---from facebook---')
+      console.log(res);
+    })
+  }
+
+  loginGoogle() {
+    this.fireAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider())
+    .then(res => {
+      console.log('---from google---')
+      console.log(res)
+    })
+  }
+
+  logout() {
+    this.fireAuth.auth.signOut()
+    .then(res => {
+      console.log('---log out result---')
+      console.log(res)
+    })
+  }
+
 
 }
